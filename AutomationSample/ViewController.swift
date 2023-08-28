64064a22bda8c2f9b0e086ff856e99a052fafdde
Vaishnavi Charde <vaishnavi.charde@tissatech.com>
2023-08-28 19:06:46 +0530

    Initial commit

---
+++ AutomationSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutomationSample
+//
+//  Created by Tissa-Tech on 28/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
